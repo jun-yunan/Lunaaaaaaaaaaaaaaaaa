f553695e278aaff6cec660e038a535de51da1edb
jun-yunan <junyunan341695@gmail.com>
2023-04-11 11:12:03 +0700

    code cua kiet

---
+++ administrator/elements_lan/mod/hanghoaCls.php
@@ -0,0 +1,62 @@
+<?php
+
+$s = '../../administrator/elements_lan/mod/database.php';
+if (file_exists($s)) {
+    $f = $s;
+} else {
+    $f = './administrator/elements_lan/mod/database.php';
+}
+require_once $f;
+class hanghoaCls extends database
+{
+    public function HanghoaGetAll()
+    {
+        $getAll = $this->connect->prepare("select * from hanghoa");
+        $getAll->setFetchMode(PDO::FETCH_OBJ);
+        $getAll->execute();
+
+        return $getAll->fetchAll();
+    }
+    public function HanghoaAdd($tenhanghoa, $mota, $giathamkhao, $tenhinhanh, $hinhanh, $idloaihang)
+    {
+        $add = $this->connect->prepare("INSERT INTO hanghoa(tenhanghoa, mota, giathamkhao, tenhinhanh, hinhanh, idloaihang)
+              VALUES (?,?,?,?,?,?)");
+        $add->execute(array($tenhanghoa, $mota, $giathamkhao, $tenhinhanh, $hinhanh, $idloaihang));
+
+        return $add->rowCount();
+    }
+    public function HanghoaDelete($idhanghoa)
+    {
+        $del = $this->connect->prepare("delete from hanghoa where idhanghoa=?");
+        $del->execute(array($idhanghoa));
+
+        return $del->rowCount();
+    }
+    public function HanghoaUpdate($tenhanghoa, $mota, $giathamkhao, $tenhinhanh, $hinhanh, $idloaihang, $idhanghoa)
+    {
+        $update = $this->connect->prepare("UPDATE hanghoa SET "
+            . "tenhanghoa=?, mota=?, giathamkhao=?, tenhinhanh=?, hinhanh=?, idloaihang=?"
+            . "WHERE idhanghoa = ?");
+        $update->execute(array($tenhanghoa, $mota, $giathamkhao, $tenhinhanh, $hinhanh, $idloaihang, $idhanghoa));
+
+        return $update->rowCount();
+    }
+    public function HanghoaGetbyId($idhanghoa)
+    {
+        $getTK = $this->connect->prepare("select * from hanghoa where idhanghoa=?");
+        $getTK->setFetchMode(PDO::FETCH_OBJ);
+        $getTK->execute(array($idhanghoa));
+
+        return $getTK->fetch();
+    }
+    public function HanghoaGetbyIdloaihang($idloaihang) {
+        $getAllId = $this->connect->prepare("select * from hanghoa where idloaihang=?");
+        if ($getAllId->execute(array($idloaihang))) {
+            $result = $getAllId->fetchAll(PDO::FETCH_OBJ);
+            return $result;
+        } else {
+            // handle error
+            return false;
+        }
+    }
+}
